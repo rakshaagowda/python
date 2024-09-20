27bd2a4123a788c47bb45e9e08831f05bf33842f
RAKSHA B R <rakshagowdah264@gmail.com>
2024-09-20 12:26:32 +0530

    added logo of the guessing game

--- numberguess.py
+++ numberguess.py
@@ -1,12 +1,27 @@
 import random
 #import art
-#print(art.logo)
+
+
+logo = """
+  / _ \_   _  ___  ___ ___  /__   \ |__   ___    /\ \ \_   _ _ __ ___ | |__   ___ _ __ 
+ / /_\/ | | |/ _ \/ __/ __|   / /\/ '_ \ / _ \  /  \/ / | | | '_ ` _ \| '_ \ / _ \ '__|
+/ /_\\| |_| |  __/\__ \__ \  / /  | | | |  __/ / /\  /| |_| | | | | | | |_) |  __/ |   
+\____/ \__,_|\___||___/___/  \/   |_| |_|\___| \_\ \/  \__,_|_| |_| |_|_.__/ \___|_| 
+"""
+
+
+print(logo)
+
+
 print("Welcome to number guessing game\nI'm thinking of a number between 1 - 100\ntry to guess it ")
+
 level=input("type 'easy' for easy level and 'hard for hard level of the game  ")
 if level == "easy":
     attempts=10
 else:
     attempts=5
+
+
 choice=random.randint(1,100)
 
 guess=0
