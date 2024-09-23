4b2b87faec40bd8998eb78204861a2f0d44da866
RAKSHA B R <rakshagowdah264@gmail.com>
2024-09-23 22:52:39 +0530

    added description about usage of debugger in fixing errors

--- fixing_errors.py
+++ fixing_errors.py
@@ -49,3 +49,28 @@ total_words = pages * word_per_page
 print(total_words)
 
 #when word_per_page is printed it is noted that there was issue with assignment operator
+
+#USING DEBUGGER
+
+"""Most IDEs (Intelligent Development Environments) such as PyCharm will have built-in tools for debugging.
+This is normally known as the debugger. In many ways, they are like print statements on steroids.
+
+Debuggers allows us to peek into our code during execution and pause on chosen lines
+ to figure out what is the inner mechanism and where it's going wrong.
+
+There are a couple of things that are the same in most IDEs which you should be familiar with:
+
+Breakpoint - You can set a breakpoint by clicking on a line in the gutter of the code (where the line numbers are).
+ This line will be where the program pauses during debug run.
+
+Step Over - This button will go through the execution of your code line by 
+ and allow you to view the intermediate values of your variables.
+
+Step Into - This will enter into any other modules that your code references. 
+e.g. If you use a function from the random module it will show you the original code for that function 
+so you can better understand its functionality and how it relates to your problems.
+
+Step Into My Code - This does the same thing as Step Into, 
+but it limits the scope to your own project code and ignores library code such as random.
+
+Use the PyCharm debugger to figure out what is the issue in the starting code and fix it."""
