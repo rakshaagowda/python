449f54e233d816b69e2276a381266a7b2c5f58e9
RAKSHA B R <rakshagowdah264@gmail.com>
2024-10-02 20:39:01 +0530

    added code

---
+++ turtle.py
@@ -0,0 +1,29 @@
+import turtle
+
+import another_module
+print(another_module.another_variable)
+
+import turtle
+"""constructing object (timmy) 'turtle' is imported and 
+from which class 'Turtle' is fetched
+timmy=turtle.Turtle()
+print(timmy)"""
+
+#or
+#attributes
+from turtle import Turtle,Screen
+
+timmy =Turtle()
+timmy.shape("turtle") #changes shape
+timmy.color("coral")#changes color
+#to make the turtle move in a triangular shape
+for i in range(0,3):
+    timmy.forward(100)
+    timmy.left(120)#method left has argument angle
+
+my_screen=Screen()
+print(my_screen.canvheight)
+
+
+#methods eg:object.mathod() just likefunction methodalong with object  doessomething
+my_screen.exitonclick()
