8fee96293500ddd4c95135020a86a6c908b732f9
RAKSHA B R <rakshagowdah264@gmail.com>
2024-10-07 23:19:42 +0530

    added mainpart of code where previously defined classes and functions are called

--- quizbrain.py
+++ quizbrain.py
@@ -57,3 +57,27 @@ class QuizBrain:
         print(f"The correct answer was:{correct_answer}")
         print(f"\Your score:({self.score}/{self.question_number}")
 
+
+"""***********main part of code*****************"""
+#from question_model import Question
+#from data import question_data
+#from quiz_brain import QuizBrain
+
+
+question_bank=[]
+for question in question_data:
+    question_text =question["text"]
+    question_answer=question["answer"]
+    new_question=Question(question_text,question_answer)
+    question_bank.append(new_question)
+
+
+quiz=QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+
+
+print("you've completed the quiz")
+print(f"Your final score was:{quiz.score}/{quiz.question_number}")
