69c0f814a2114e3b82db9f17495e36132db8ada2
RAKSHA B R <rakshagowdah264@gmail.com>
2024-10-09 11:27:22 +0530

    added code

--- turtlegraphics.py
+++ turtlegraphics.py
@@ -1,1 +1,11 @@
 """draing a square using turtlr graphics module"""
+from turtle import Turtle,Screen
+tim=Turtle()
+tim.shape("turtle")
+tim.color("blue")
+
+for _ in range(4):
+    tim.forward(100)
+    tim.left(90)
+
+
