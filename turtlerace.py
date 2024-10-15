e717abef7358bbc19593720d9e11cfd5255d74cb
RAKSHA B R <rakshagowdah264@gmail.com>
2024-10-15 23:41:42 +0530

    added code

---
+++ turtlerace.py
@@ -0,0 +1,12 @@
+import turtle
+
+tim=turtle.Turtle()
+
+screen=turtle.Screen()
+
+
+def move_forward():
+    tim.forward(10)
+screen.listen()
+screen.onkey(key="space",fun=move_forward)
+screen.exitonclick()
