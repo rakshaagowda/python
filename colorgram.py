c250a743499152749922d6664953280e6e7b970f
RAKSHA B R <rakshagowdah264@gmail.com>
2024-10-15 22:47:22 +0530

    added code

--- colorgram.py
+++ colorgram.py
@@ -3,3 +3,15 @@ import colorgram
 colors=colorgram.extract('image.jpeg',30)
 print(colors)
 """
+
+import colorgram
+rgb_colors=[]
+colors=colorgram.extract('image.jpeg',30)
+for color in colors:
+    r=color.rgb.r
+    g=color.rgb.g
+    b=color.rgb.b
+    new_color=(r,g,b)
+    rgb_colors.append(new_color)
+
+print(rgb_colors)
