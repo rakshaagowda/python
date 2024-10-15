19565c9d0374d9d2478284a7ce2c758002f3e2a3
RAKSHA B R <rakshagowdah264@gmail.com>
2024-10-15 23:57:15 +0530

    all the higher order functions are defined which controls the movement of turtle

--- turtlerace.py
+++ turtlerace.py
@@ -8,6 +8,32 @@ screen=turtle.Screen()
 
 def move_forward():
     tim.forward(10)
+
+def move_backward():
+    tim.backward(10)
+
+def turn_left():
+    new_heading=tim.heading()+10
+    tim.setheading(new_heading)
+    # tim.left(10)
+
+
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+    #tim.right(10)
+
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
 screen.listen()
-screen.onkey(key="space",fun=move_forward)
+screen.onkey(move_forward,"w")
+screen.onkey(move_backward,"s")
+screen.onkey(turn_right,"a")
+screen.onkey(turn_left,"d")
+screen.onkey(clear,"c")
+
 screen.exitonclick()
