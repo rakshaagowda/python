7522aaec92a56e5c297b926b7e4070245b3df794
RAKSHA B R <rakshagowdah264@gmail.com>
2024-10-16 22:56:12 +0530

    added code

---
+++ turtlerace.py
@@ -0,0 +1,19 @@
+import turtle
+import random
+
+
+screen=turtle.Screen()
+
+
+screen.setup(width=500,height=400)
+user_bet=screen.textinput(title="Make your bet",prompt="which turtle do you think would win the race? enter a colour: ")
+colors=["red","orange","yellow","green","blue","purple"]
+for turle_index in range(0,5):
+    tim = turtle.Turtle()
+    tim.color(colors[turtle_index])
+    tim.penup()
+    tim.shape("turtle")
+    tim.goto(x=-238, y=y_positions)
+
+
+screen.exitonclick()
