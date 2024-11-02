ff94c03b27bcbe3008066a744011f982bdedabe5
RAKSHA B R <rakshagowdah264@gmail.com>
2024-11-02 08:36:47 +0530

    Added code

--- classinheritance.py
+++ classinheritance.py
@@ -3,10 +3,29 @@
 a class can have methods and attributes defined in another class usimg class inheritance
 """
 
-"""eg: class Fish(Animal):
+class Animal():
+   def __init__(self):
+    def numeyes(self):
+         print("num of eyes: 2")
+
+    def breathe(self):
+                print("inhale,exhale")
+                  
+class Fish(Animal):
             def __init__(self):
                 super().__init__()
+            def swim(self):
+                  print("moving in water")
+
+            #can modify methods of super class in inherited class 
+            def breathe(self):
+                  super.breathe()
+                  print("doing this underwater")
+                  
+                   
+
+nemo=Fish()
+nemo.swim()#method of Fish class
+nemo.breathe()#method inherited from Animal class                
                 
-                
-                """
 #class Fish is inherited methods and attributes from class Animal
