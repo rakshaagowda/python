5cd4a889f4b50a8d3b97dea5723bef53bcdb9a0e
RAKSHA B R <rakshagowdah264@gmail.com>
2024-11-02 08:21:12 +0530

    Added code

---
+++ classinheritance.py
@@ -0,0 +1,12 @@
+"""class inheritence
+
+a class can have methods and attributes defined in another class usimg class inheritance
+"""
+
+"""eg: class Fish(Animal):
+            def __init__(self):
+                super().__init__()
+                
+                
+                """
+#class Fish is inherited methods and attributes from class Animal
